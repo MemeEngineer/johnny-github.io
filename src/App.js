c8b4b5321472512304086ea04adccd65bf6087ce
JWu <johnnyw131@gmail.com>
2023-08-17 00:56:58 -0400

    navbar layout

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Navbar from "./components/Navbar"
 import About from "./pages/About"
 import Gallery from "./pages/Gallery"
 import Contact from "./pages/Contact"
+import Labs from "./pages/Labs"
+import Memepage from "./pages/Memepage"
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      
       <Navbar/>
       <Routes>
 
     <Route path="/" element={<About/>} />
     <Route path="/gallery" element={<Gallery/>}/>
     <Route path="/contact" element={<Contact/>}/>
+    <Route path="/labs" element={<Labs/>}/>
+    <Route path="/memes" element={<Memepage/>}/>
     <Route path="*" element={<About/>}/>
 
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,71 @@
-import {Link} from "react-router-dom"
-import {useState} from "react"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-  } from "react-pro-sidebar";
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
 import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import {
+  FiHome,
+  FiLogOut,
+  FiArrowLeftCircle,
+  FiArrowRightCircle,
+} from "react-icons/fi";
 import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Navbar.css";
+import JohnnyWu from "../assets/JohnnyWu.gif";
+import JWLOGO from "../assets/JWLOGO.png";
 
-export default function Navbar(){
-     //create initial menuCollapse state using useState hook
-     const [menuCollapse, setMenuCollapse] = useState(false)
+export default function Navbar() {
+  //create initial menuCollapse state using useState hook
+  const [menuCollapse, setMenuCollapse] = useState(false);
 
-     //create a custom function that will change menucollapse state from false to true and true to false
-   const menuIconClick = () => {
-     //condition checking to change state from true to false and vice versa
-     menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-   };
- 
-    return(
-        <nav>
-            
+  //create a custom function that will change menucollapse state from false to true and true to false
+  const menuIconClick = () => {
+    //condition checking to change state from true to false and vice versa
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+  };
+
+  return (
+    <nav>
       <div id="header">
-          {/* collapsed props to change menu size using menucollapse state */}
+        {/* collapsed props to change menu size using menucollapse state */}
         <ProSidebar collapsed={menuCollapse}>
           <SidebarHeader>
-          <div className="logotext">
+            <div className="logotext">
               {/* small and big change using menucollapse state */}
-              <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
+              <p>
+                {menuCollapse ? <img src={JWLOGO} alt="JW Logo" /> : <img src={JohnnyWu} alt="JohnnyWu" />}
+              </p>
             </div>
             <div className="closemenu" onClick={menuIconClick}>
-                {/* changing menu collapse icon on click */}
-              {menuCollapse ? (
-                <FiArrowRightCircle/>
-              ) : (
-                <FiArrowLeftCircle/>
-              )}
+              {/* changing menu collapse icon on click */}
+              {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
             </div>
           </SidebarHeader>
           <SidebarContent>
-            <Menu iconShape="square">
-              <MenuItem active={true} icon={<FiHome />}>
-                Home
+            <Menu>
+              <MenuItem active={false} icon={<FaRegHeart />}>
+                <Link to="/">About</Link>
               </MenuItem>
-              <MenuItem icon={<FaList />}>Category</MenuItem>
-              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+              <MenuItem icon={<FaList />}>
+                <Link to="/gallery">Gallery</Link>
+              </MenuItem>
+              <MenuItem active={false} icon={<RiPencilLine />}>
+                <Link to="/contact">Contacts</Link>
+              </MenuItem>
+              <MenuItem icon={<BiCog />}>
+                <Link to="/labs">Labs</Link>
+              </MenuItem>
+              <MenuItem icon={<BiCog />}>
+                <Link to="/memes">Memes</Link>
+              </MenuItem>
             </Menu>
           </SidebarContent>
           <SidebarFooter>
@@ -63,7 +75,6 @@ export default function Navbar(){
           </SidebarFooter>
         </ProSidebar>
       </div>
-        </nav>
-        
-    )
+    </nav>
+  );
 }
