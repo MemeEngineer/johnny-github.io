2ed391be5e83d12c98cb79bfc5723c9ae2326aea
JWu <johnnyw131@gmail.com>
2023-08-20 20:03:27 -0400

    Gallery Styling

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -7,6 +7,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+// import Button from '@mui/material/Button';
 import "./Gallery.css";
 
 export default function Gallery(props) {
